a2b594d716b73122621bf4037ccd85c2ec1eff41
Victor Benning <victor.benning@clearscore.com>
2022-03-09 14:41:16 +0000

    make app use json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { json } from 'express';
 import routes from './routes'
 
 const app = express();
+app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(routes);
+
 
 app.listen(8080, async () => {
   console.log('🚀 server started on port 8080!');
