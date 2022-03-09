f710240c0d54df4f98f230496b78d313d72ab7f3
Victor Benning <victor.benning@clearscore.com>
2022-03-09 15:08:35 +0000

    testing first iteration

--- src/controllers/QueueController.ts
+++ src/controllers/QueueController.ts
@@ -1,12 +1,22 @@
 import { NextFunction, Request, Response } from "express";
+import axios from "axios";
+
 var developQueue: any[] = [];
 
 export default {
   async event(req: Request, res: Response, next: NextFunction) {
     let payload = req.body;
-    res.sendStatus(200);
 
-    developQueue.push(req.body);
-    console.log({ queue: developQueue, payload})
+    if (payload.command == '/claim') { 
+      developQueue.push(req.body.user_name);
+    }
+    else if (payload.command == '/done') { 
+      const user = payload.user_name;
+      developQueue = developQueue.filter(u => u != user);
+    }
+
+    
+    console.log({ queue: developQueue, payload});
+    res.send(developQueue);
   },
 };
