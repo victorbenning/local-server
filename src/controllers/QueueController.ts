eabfa0de9b90b1a7a2a90998d04c579d09c3ff36
Victor Benning <victor.benning@clearscore.com>
2022-03-09 14:21:55 +0000

    trying to print payload

--- src/controllers/QueueController.ts
+++ src/controllers/QueueController.ts
@@ -3,7 +3,7 @@ var developQueue: any[] = [];
 
 export default {
   async event(req: Request, res: Response) {
-    developQueue.push(req.body.event);
+    developQueue.push(req.body);
     return res.json({ queue: developQueue, message: req.body.event });
   },
 };
