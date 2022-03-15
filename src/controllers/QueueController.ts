07d79129a9d5f83563e95763d3f4e09e98e18736
Victor Benning <victor.benning@clearscore.com>
2022-03-15 10:21:58 -0300

    [CS-77708] removing console logs

--- src/controllers/QueueController.ts
+++ src/controllers/QueueController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import Queue from "../models/Queue";
 
+// TODO: send message when user gets turn into queue
 var developQueue: Queue[] = [];
 
 export default {
@@ -9,15 +10,11 @@ export default {
     
     let item = new Queue();
     let instructions = payload.text.split(" ", 2);
-    console.log(instructions);
     Object.assign(item, { 
       user_name: payload.user_name,
       pr_number: instructions[0]
     });
 
-    console.log(item);
-
-    
     if(instructions[1] === '--priority') { 
       developQueue.splice(0, 0, item);
     } else { 
@@ -33,20 +30,15 @@ export default {
 
   async done(req: Request, res: Response, next: NextFunction) {
     let payload = req.body
-    console.log(payload);
-
     let pr_number = payload.pr_id;
     let user_name = payload.user_name;
-    console.log(`pr_number: ${pr_number}: user_name: ${user_name}`);
     if(pr_number != undefined) { 
       developQueue = developQueue.filter((u) => u.pr_number != pr_number);
     } else { 
-      console.log(developQueue)
       const index = developQueue.findIndex((u) => u.user_name === user_name);
       if(index >= 0) { 
         developQueue.splice(index, 1);
       }
-      console.log(`index: ${index}`);
     }
   
     res.send(developQueue.map( item => item.user_name));
